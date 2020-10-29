44b81620dbdb8d7d3529211d79b8a8abec8f51fd
Simei Yeh <simei.yeh@gmail.com>
2020-10-28 22:44:49 -0700

    Updated users

--- server/models/users.js
+++ server/models/users.js
@@ -1,6 +1,16 @@
 var db = require('../db');
 
 module.exports = {
-  getAll: function () {},
+  getAll: function (callback) {
+    db.getAllUsers((err, data) => {
+      if (err) {
+        callback(err);
+      } else {
+        callback(null, data);
+      }
+    })
+  },
+
+  
   create: function () {}
 };
